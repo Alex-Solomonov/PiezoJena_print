192bc0a21d98b59f455608ba684ee51f90af1b55
Alex Solomonov <solomonoff.alexandr@gmail.com>
2021-02-03 17:23:12 +0300

    Add function for loading image as thumbnail and find coordinate of pixel
    Add function of loading image as thumbnail. It's allow to load image with big resolution.
    
    Add function of searhing coordinate.

--- piezo_main.py
+++ piezo_main.py
@@ -1,7 +1,29 @@
 import tools.controller as controller
-
+import tools.data as data
+import numpy as np
+import matplotlib.pyplot as plt
 # Parameters
 com_port = 'COM3' #See at Computer Management/Ports
 
-#
-controller.connect(com_port, True)
+#Exposure time in seconds
+delay_time = 0.1
+#Longest side length
+length_of_side = 40. #mkm
+
+image_name = 'D:\\piezo_controller\\PiezoJena_print\\example\\logo.png'
+size = 128, 128
+
+	
+# controller.connect(com_port, True)
+
+image = data.load_image_as_thumbnail(image_name, size)
+# image = data.load_image(image_name) #for small image only
+
+listofcoord =  data.get_coordinates(image, length_of_side)
+print(listofcoord)
+
+plt.plot(listofcoord[:,0], listofcoord[:,1])
+plt.plot(listofcoord[:,0], listofcoord[:,1], '*')
+plt.show()
+
+# controller.draw(com_port, listofcoord, delay_time)
--- tools/data.py
+++ tools/data.py
@@ -21,7 +21,8 @@ __email__ = "solomonoff.alexandr@gmail.com"
 
 def load_image(file_path: str) -> np.ndarray:
 	'''
-	Load RGB image and convert into grayscale array
+	Load RGB image and convert into normalize grayscale array
+
 	Parameters
 	----------
 	file_path : string
@@ -33,7 +34,79 @@ def load_image(file_path: str) -> np.ndarray:
 		array in grayscale format
 
 	'''
-	
+
 	img = Image.open(file_path)# type(img) -- <class 'PIL.*ImagePlugin.*ImageFile'
 	img_data = np.array(grayscale(img)) # type(img_data) -- <class 'numpy.ndarray'>
-	return img_data
+	
+	return img_data/np.max(img_data)
+
+def load_image_as_thumbnail(file_path: str, size: tuple) -> np.ndarray:
+	'''
+	Load RGB image and convert into normalize grayscale array as thumbnail
+
+	Parameters
+	----------
+	file_path : string
+		File name of the image which contain name, fileformat and path
+
+	Returns
+	-------
+	img_data : ndarray
+		array in grayscale format
+
+	'''
+	img = Image.open(file_path)# type(img) -- <class 'PIL.*ImagePlugin.*ImageFile'
+	img = grayscale(img)# convert RGB image to grayscale
+	img.thumbnail(size)
+	img_data = np.array(img)
+	'''
+	TODO:
+
+	Find optimal algorithm of removing gray 
+	'''
+	img_data[img_data >= 122.5] = 255
+	img_data[img_data <= 122.5] = 0
+
+	return img_data/np.max(img_data)
+
+
+def get_coordinates(array: np.ndarray, length: float = 80.) -> np.ndarray:
+	'''
+	Get coordinate of offset from zero pont
+
+	Parameters
+	----------
+	array : ndarray
+		An image in the form of a matrix of binary values
+
+	length : optional, float
+		The length of the longest side of the image in physical space
+
+	Returns
+	-------
+	coordinates : ndarray
+		The coordinates to which the piezocontroller should move
+
+	'''
+
+	shape = np.shape(array) 
+
+	#Set scale to other size of image in physical space
+	scale = shape[1]/shape[0]
+	if scale <= 1:
+		y_length  = scale*length
+	else:
+		y_length = length/scale
+
+	#Step of piezocontroller
+	dx = length/shape[1]
+	dy = y_length/shape[0]
+
+	#Searches for indices of black pixels in the image
+	row_coord, col_coord = np.where(array == 0)
+	coordinates = np.zeros([np.size(row_coord), 2])
+	for i in range(np.size(row_coord)):
+		coordinates[i, 0] = col_coord[i]*dx
+		coordinates[i, 1] = (shape[0] - row_coord[i])*dy
+
+	return coordinates
